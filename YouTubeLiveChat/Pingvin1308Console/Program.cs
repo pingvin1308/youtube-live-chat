b543ed5b79100be2d6f3a8fdbec291d4a4195d9f
Trufanov Roman <pingvin1308@gmail.com>
2021-03-27 17:16:04 +0300

    init projects

---
+++ YouTubeLiveChat/Pingvin1308Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pingvin1308Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
